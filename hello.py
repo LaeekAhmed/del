e6567919598ac45815169fc27540b7bb76c36200
LaeekAhmed <laeek385@gmail.com>
2023-11-22 12:03:22 -0500

    Fix indentation in hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
 
 def add(x, y):
-    return x + y
+   return x + y
 
 def subtract(x, y):
-    return x - y
+   return x - y
 
 def multiply(x, y):
-    return x * y
+   return x * y
 
 def divide(x, y):
-    return x / y
+   return x / y
 
 # Example usage
 result = add(5, 3)
